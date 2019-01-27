424d55bd7cb836724b676335f625421f292dbe3f
carlogilmar <carlogilmar12@gmail.com>
2019-01-27 15:44:38 -0600

    Adding test for fing neighbours

--- lib/gol/gol.ex
+++ lib/gol/gol.ex
@@ -8,4 +8,45 @@ defmodule GameOfLife do
       [:c13, :c14, :c15, :c16]
     ]
   end
+
+  def get_neighbors( population, {x,y} ) do
+    up_neighbors = population |> get_up_neighbors(x,y)
+    middle_neighbors = population |> get_middle_neighbors(x,y)
+    down_neighbors = population |> get_down_neighbors(x,y)
+    up_neighbors ++ middle_neighbors ++ down_neighbors
+  end
+
+  def get_up_neighbors( population, x, y ) do
+    row = Enum.at( population, y-1 )
+    case Enum.at( row, x+1 ) do
+      nil ->
+        [h|_t] = row
+        [ Enum.at( row, x-1 ), Enum.at( row, x ), h ]
+      _ -> [ Enum.at( row, x-1 ), Enum.at( row, x ), Enum.at( row, x+1 ) ]
+    end
+  end
+
+  def get_middle_neighbors( population, x, y ) do
+    row = Enum.at( population, y )
+    case Enum.at( row, x+1 ) do
+      nil ->
+        [h|_t] = row
+        [ Enum.at( row, x-1 ), h ]
+      _ ->
+        [ Enum.at( row, x-1 ), Enum.at( row, x+1 ) ]
+    end
+  end
+
+  def get_down_neighbors( population, x, y ) do
+    row = Enum.at( population, y+1 )
+    case row do
+      nil ->
+        row = Enum.at( population, 0 )
+        [h|_t] = row
+        [ Enum.at( row, x-1 ), Enum.at( row, x ), h ]
+      _ ->
+        [ Enum.at( row, x-1 ), Enum.at( row, x ), Enum.at( row, x+1 ) ]
+    end
+  end
+
 end
--- test/gol_test.exs
+++ test/gol_test.exs
@@ -7,4 +7,21 @@ defmodule GameOfLifeTest do
     population = length( init_population )
     assert population == 4
   end
+
+  test "Get neighbors from cell" do
+    population = GameOfLife.fake_population()
+    cell1 = {0,0}
+    cell2 = {1,1}
+    cell3 = {2,2}
+    cell4 = {3,3}
+    neighbouts1 = GameOfLife.get_neighbors( population, cell1 )
+    neighbouts2 = GameOfLife.get_neighbors( population, cell2 )
+    neighbouts3 = GameOfLife.get_neighbors( population, cell3 )
+    neighbouts4 = GameOfLife.get_neighbors( population, cell4 )
+    assert neighbouts1 == [:c16, :c13, :c14, :c4, :c2, :c8, :c5, :c6]
+    assert neighbouts2 == [:c1, :c2, :c3, :c5, :c7, :c9, :c10, :c11]
+    assert neighbouts3 == [:c6, :c7, :c8, :c10, :c12, :c14, :c15, :c16]
+    assert neighbouts4 == [:c11, :c12, :c9, :c15, :c13, :c3, :c4, :c1]
+  end
+
 end
