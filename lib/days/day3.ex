7ea39f424e370d71437f019ccc8b50274187b9f7
carlogilmar <carlogilmar12@gmail.com>
2019-01-31 16:18:01 -0600

    Adding test for parse line in sizes

--- lib/days/day3.ex
+++ lib/days/day3.ex
@@ -9,4 +9,16 @@ defmodule AdventOfCode.Day3 do
     fill_matriz( size, index+1, size==index+1, new_matriz)
   end
 
+	def draw_rectangle_in_matriz( matriz, rectangle ) do
+		{x,y,w,h} = get_sizes( rectangle )
+	end
+
+	def get_sizes( rectangle ) do
+    [_id, _at, xy, wh] = rectangle |> String.split(" ")
+		[w, h] = wh |> String.split("x")
+		[xy_parsed, _] = xy |> String.split ":"
+		[x, y] = xy_parsed |> String.split(",")
+		{ String.to_integer(x), String.to_integer(y), String.to_integer(w), String.to_integer(h)}
+	end
+
 end
--- test/days/day3_test.exs
+++ test/days/day3_test.exs
@@ -8,4 +8,39 @@ defmodule AdventOfCodeTest.Day1Test do
     assert m == m_expected
   end
 
+	test "Get the sizes from string line" do
+		line1 = "#15 @ 823,32: 23x26"
+		line2 = "#16 @ 124,118: 20x18"
+		line3 = "#17 @ 675,632: 22x26"
+		w_line1 = {823, 32, 23, 26}
+		w_line2 = {124, 118, 20, 18}
+		w_line3 = {675, 632, 22, 26}
+		size_from_line1 = Day3.get_sizes( line1 )
+		size_from_line2 = Day3.get_sizes( line2 )
+		size_from_line3 = Day3.get_sizes( line3 )
+		assert size_from_line1 == w_line1
+		assert size_from_line2 == w_line2
+		assert size_from_line3 == w_line3
+	end
+
+	#test "Draw a rectangle in matrix" do
+	#	rectangle = "#123 @ 3,2: 5x4"
+	#	new_matriz = Day3.build_matriz( 11 ) |> draw_rectangle_in_matriz( rectangle )
+	#	m_expected =
+	#		[
+	#		  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+	#		  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+	#		  [".", ".", ".", "#", "#", "#", "#", "#", ".", ".", "."],
+	#		  [".", ".", ".", "#", "#", "#", "#", "#", ".", ".", "."],
+	#		  [".", ".", ".", "#", "#", "#", "#", "#", ".", ".", "."],
+	#		  [".", ".", ".", "#", "#", "#", "#", "#", ".", ".", "."],
+	#		  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+	#		  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+	#		  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+	#		  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+	#		  [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+	#		]
+	#	assert new_matriz == m_expected
+	#end
+
 end
